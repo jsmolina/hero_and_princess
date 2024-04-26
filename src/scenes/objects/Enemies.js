6a161f6a9feacfea28e251c35c186f2dcdef64c2
jsmolina <jordi.sesmeromolina@telefonica.com>
2024-04-26 10:27:10 +0200

    chore: order monkey positions

--- src/scenes/objects/Enemies.js
+++ src/scenes/objects/Enemies.js
@@ -71,112 +71,117 @@ class Enemies {
         {noAction: "pos1"},
       ),
     }
+    this.monkeyFsm = [
+      {pos: "left", arm: ""},
+      {pos: "left", arm: ""}
+    ]
     this.monkey = {
-      pos24: utils.addOthers(
-        {x: 120, y: 130, frame: 20},
+      left: utils.addOthers(
+        {x: 120, y: 130, frame: 20, visible: false},
         {}
       ),
-      pos25: utils.addOthers(
+      middle: utils.addOthers(
         {x: 285, y: 100, frame: 21},
         {}
       ),
-      pos26: utils.addOthers(
-        {x: 430, y: 55, frame: 22, scale: 0.22},
+      right: utils.addOthers(
+        {x: 430, y: 55, frame: 22, scale: 0.22, visible: true},
         {}
       ),
     };
     this.monkeyArm = {
-      pos24: utils.addOthers(
-        {x: 194, y: 175, frame: 33},
-        {}
-      ),
-      pos24_1: utils.addOthers(
-        {x: 115, y: 150, frame: 35},
-        {}
-      ),
-      pos25: utils.addOthers(
-        {x: 270, y: 125, frame: 35},
-        {}
-      ),
-      pos25_1: utils.addOthers(
-        {x: 342, y: 145, frame: 33},
-        {}
-      ),
-      pos26: utils.addOthers(
-        {x: 435, y: 100, frame: 42, scale: 0.3},
-        {}
-      ),
-      pos26_2: utils.addOthers(
-        {x: 465, y: 70, frame: 41, scale: 0.3},
-        {}
-      ),
-      pos26_3: utils.addOthers(
-        {x: 480, y: 92, frame: 43, scale: 0.3},
-        {}
-      ),
-    };
-    this.ball = {
-      pos1: utils.addOthers(
-        {x: 190, y: 380, frame: 37},
-        {noAction: "pos2"}
-      ),
-      pos2: utils.addOthers(
-        {x: 170, y: 480, frame: 37},
-        {noAction: "pos3"}
-      ),
-      pos3: utils.addOthers(
-        {x: 120, y: 610, frame: 37},
-        {noAction: "pos1"}
-      ),
-      pos17: utils.addOthers(
-        {x: 580, y: 340, frame: 37},
-        {}
-      ),
-      pos18: utils.addOthers(
-        {x: 480, y: 340, frame: 37},
-        {}
-      ),
-      pos19: utils.addOthers(
-        {x: 385, y: 330, frame: 37},
-        {}
-      ),
-      pos20: utils.addOthers(
-        {x: 255, y: 310, frame: 37},
-        {}
-      ),
-      pos20_1: utils.addOthers(
-        {x: 285, y: 280, frame: 37},
-        {}
-      ),
-      pos20_2: utils.addOthers(
-        {x: 285, y: 230, frame: 39},
-        {}
-      ),
-      pos20_3: utils.addOthers(
-        {x: 290, y: 320, frame: 39},
-        {}
-      ),
-      pos21: utils.addOthers(
-        {x: 145, y: 320, frame: 37},
-        {}
-      ),
-      pos22: utils.addOthers(
-        {x: 170, y: 250, frame: 39},
-        {}
-      ),
-      pos23: utils.addOthers(
-        {x: 45, y: 260, frame: 39},
-        {}
-      ),
-      pos24_2: utils.addOthers(
+      leftTake: utils.addOthers(
         {x: 170, y: 135, frame: 34},
         {}
       ),
-      pos25_2:
+      leftDrop: utils.addOthers(
+        {x: 194, y: 175, frame: 33},
+        {}
+      ),
+      leftPunch: utils.addOthers(
+        {x: 115, y: 150, frame: 35},
+        {}
+      ),
+      middlePunch: utils.addOthers(
+        {x: 270, y: 125, frame: 35},
+        {}
+      ),
+      middleDrop: utils.addOthers(
+        {x: 342, y: 145, frame: 33},
+        {}
+      ),
+      middleTake:
         utils.addOthers(
           {x: 320, y: 110, frame: 34},
           {}
-        ),
+      ),
+      rightPunch: utils.addOthers(
+        {x: 435, y: 100, frame: 42, scale: 0.3},
+        {noAction: "right_1"}
+      ),
+      rightAngry: utils.addOthers(
+        {x: 465, y: 70, frame: 41, scale: 0.3},
+        {noAction: "right_2"}
+      ),
+      rightTake: utils.addOthers(
+        {x: 470, y: 82, frame: 43, scale: 0.3, visible: true},
+        {noAction: "right"}
+      ),
+    };
+    this.ball = {
+      downScreenTop: utils.addOthers(
+        {x: 190, y: 380, frame: 37},
+        {noAction: "downScreenMiddle"}
+      ),
+      downScreenMiddle: utils.addOthers(
+        {x: 170, y: 480, frame: 37},
+        {noAction: "downScreenMiddle"}
+      ),
+      downScreenBottom: utils.addOthers(
+        {x: 120, y: 610, frame: 37},
+        {noAction: "downScreenBottom"}
+      ),
+      topScreenRight3: utils.addOthers(
+        {x: 580, y: 340, frame: 37, visible: false},
+        {}
+      ),
+      topScreenRight2: utils.addOthers(
+        {x: 480, y: 340, frame: 37, visible: false},
+        {}
+      ),
+      topScreenRight1: utils.addOthers(
+        {x: 385, y: 330, frame: 37, visible: false},
+        {}
+      ),
+      topScreenRight0: utils.addOthers(
+        {x: 290, y: 320, frame: 39},
+        {}
+      ),
+      topScreenFallsMiddle1: utils.addOthers(
+        {x: 285, y: 230, frame: 39, visible: false},
+        {noAction: "topScreenFallsMiddle2"}
+      ),
+      topScreenFallsMiddle2: utils.addOthers(
+        {x: 285, y: 280, frame: 37, visible: false},
+        {noAction: "downScreenTop"}
+      ),
+      topScreenLeft1: utils.addOthers(
+        {x: 255, y: 310, frame: 37, visible: false},
+        {}
+      ),
+      topScreenLeft2: utils.addOthers(
+        {x: 145, y: 320, frame: 37, visible: false},
+        {}
+      ),
+      topScreenFallsLeft: utils.addOthers(
+        {x: 170, y: 250, frame: 39, visible: false},
+        {}
+      ),
+      topScreenOverSkull: utils.addOthers(
+        {x: 45, y: 260, frame: 39, visible: false},
+        {}
+      ),
+
     }
     this.allMonkeyPositions = Object.keys(this.monkey);
     this.allMonkeyArmPositions = Object.keys(this.monkeyArm);
