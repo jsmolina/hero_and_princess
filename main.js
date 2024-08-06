14619f6372e0e5c6470ccb94e32f29372f05dbb6
jsmolina <jordi.sesmeromolina@telefonica.com>
2024-08-06 16:35:00 +0200

    clean deps

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ function createWindow() {
       nodeIntegration: true,
     },
   });
+  mainWindow.setMenuBarVisibility(false)
 
   // Hot reload
   if (process.env.ELECTRON_HOT === "true") {
@@ -50,9 +51,9 @@ app.on("ready", createWindow);
 app.on("window-all-closed", () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
+  //if (process.platform !== "darwin") {
     app.quit();
-  }
+  //}
 });
 
 app.on("activate", () => {
